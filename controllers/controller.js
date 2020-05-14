73e2c4f46c356539dd57b6ca2d499e9d3984c239
basedantoni <anthony.mercado300@gmail.com>
2020-05-14 18:36:21 -0500

    deleted mvc examples

--- controllers/controller.js
+++ controllers/controller.js
@@ -13,31 +13,5 @@ exports.renderHomePage = (req, res) => {
         })
 }
 exports.renderSkillsPage = (req, res) => res.render('skills')
-exports.renderResumePage = (req, res) => res.render('resume')
 exports.renderContactPage = (req, res) => res.render('contact')
 exports.renderPortfolioPage = (req, res) => res.render('portfolio')
-
-// Get Pokemon
-exports.getPokemon = (req, res) => {
-    const pokemon = req.body.pokemon.toLowerCase();
-    const sprite = new Sprite(req.body.pokemon.toLowerCase());
-    
-    sprite.validateUserInput();
-
-    if(sprite.errors.length) {
-        res.render('portfolio', {
-            error: sprite.errors.toString()
-        })
-    } else {
-        // Axios GET request to PokeAPI for sprite link
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon}/`)
-        .then((response) => {
-            const { front_default: front, back_default: back } = response.data.sprites;
-            res.render('portfolio', {
-                front: front,
-                back: back
-            })
-        })
-        .catch(error => console.log(error))
-    }
-}
--- router.js
+++ router.js
@@ -3,12 +3,8 @@ const router = express.Router();
 const controller = require('./controllers/controller')
 
 router.get('/', controller.renderHomePage);
-router.get('/resume', controller.renderResumePage);
 router.get('/skills', controller.renderSkillsPage);
 router.get('/contact', controller.renderContactPage);
 router.get('/portfolio', controller.renderPortfolioPage);
-
-// Get Sprite
-router.post('/portfolio', controller.getPokemon);
 
 module.exports = router;
