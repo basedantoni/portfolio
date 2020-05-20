e388a371016bbb6add7711ddcaf862d31ac078b3
basedantoni <anthony.mercado300@gmail.com>
2020-05-20 15:49:16 -0500

    skills from api work

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,11 +2,18 @@ const axios = require('axios')
 
 // Renders pages
 exports.renderHomePage = (req, res) => {
-    axios.get('https://gitconnected.com/v1/portfolio/basedantoni')
-        .then((response) => {
-            const { picture, label, headline, summary, region } = response.data.basics
-            res.render('index', {
-                picture: picture,
-            })
-        })
+  axios.get('https://gitconnected.com/v1/portfolio/basedantoni')
+      .then((response) => {
+          const { picture, label, headline, summary, region } = response.data.basics
+          const skills = response.data.skills
+          console.log(skills)
+
+          //Turn skills into an array
+          const skillsArr = Object.values(skills).map( skill => skill.name )
+
+          res.render('index', {
+              picture: picture,
+              skills: skills,
+          })
+      })
 }
