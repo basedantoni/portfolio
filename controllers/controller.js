aad60c6e7412221de0133bec5f1ee4f17c3f038c
basedantoni <anthony.mercado300@gmail.com>
2020-06-01 23:32:30 -0500

    portfolio card images

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,16 +3,16 @@ const axios = require('axios')
 // Renders pages
 exports.renderHomePage = (req, res) => {
   axios.get('https://gitconnected.com/v1/portfolio/basedantoni')
-      .then((response) => {
+        .then((response) => {
           const { picture, label, headline, summary, region } = response.data.basics
           const skills = response.data.skills
           const projects = response.data.projects
-          console.log(projects)
-
+					console.log(projects[0].images[0].resolutions.thumbnail.url)
           res.render('index', {
               picture: picture,
               skills: skills,
-              projects: projects,
+							projects: projects,
+							thumbnail: projects[0].images[0].resolutions.micro.url,
           })
       })
 }
