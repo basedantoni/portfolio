28296f21b89bdfe8904b571cdb7679df125abb0c
basedantoni <anthony.mercado300@gmail.com>
2020-06-01 23:33:35 -0500

    commented for retrieval of image urls

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,13 +6,15 @@ exports.renderHomePage = (req, res) => {
         .then((response) => {
           const { picture, label, headline, summary, region } = response.data.basics
           const skills = response.data.skills
-          const projects = response.data.projects
+					const projects = response.data.projects
+					
+					// How to access thumbnail image for projects
 					console.log(projects[0].images[0].resolutions.thumbnail.url)
+
           res.render('index', {
               picture: picture,
               skills: skills,
 							projects: projects,
-							thumbnail: projects[0].images[0].resolutions.micro.url,
           })
       })
 }
