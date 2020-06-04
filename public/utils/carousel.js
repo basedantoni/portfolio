a4e34b34c957f1c8aed6abf61b7d46221b26a8f6
basedantoni <anthony.mercado300@gmail.com>
2020-06-03 23:30:17 -0500

    initial style complete

--- public/utils/carousel.js
+++ public/utils/carousel.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('.card-container').slick({
     // prev arrow
-    prevArrow:'<button type="button" class="slick-prev slick-arrow"></button>',
+    prevArrow:'<button type="button" class="slick-prev slick-arrow"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18.4 20.8L13.6 16L18.4 11.2" stroke="#FF7597" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
     // next arrow
-    nextArrow:'<button type="button" class="slick-next slick-arrow"></button>',
+    nextArrow:'<button type="button" class="slick-next slick-arrow"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.6 20.8L18.4 16L13.6 11.2" stroke="#FF7597" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
   });
 });
 
