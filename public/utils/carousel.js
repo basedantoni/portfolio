b32e15ec6b4eaff979ce285d8c987e81e6398cd7
basedantoni <anthony.mercado300@gmail.com>
2020-06-03 23:58:37 -0500

    mobile style done

--- public/utils/carousel.js
+++ public/utils/carousel.js
@@ -6,5 +6,3 @@ $(document).ready(function(){
     nextArrow:'<button type="button" class="slick-next slick-arrow"><svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.6 20.8L18.4 16L13.6 11.2" stroke="#FF7597" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></button>',
   });
 });
-
-$('.slick-arrow').prop('value', '');
