d7eab3207db19edbf09fa9215cf8e330ccf9f6fc
basedantoni <anthony.mercado300@gmail.com>
2020-08-31 17:48:38 -0500

    :fire: Refactored to Next.js
    Move code from Vanilla JS to Next.js

--- public/utils/assignUrl.js
+++
@@ -1,7 +0,0 @@
-// Assigns the url property to an object
-exports.assignUrl = project => {
-  project['image-url'] = project.images
-              .flat()
-              .map(item => item.resolutions.thumbnail.url)
-}
-
