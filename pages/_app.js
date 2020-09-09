7d14fc33ed766256c1e9d73bec33ac48a3c94bb0
basedantoni <anthony.mercado300@gmail.com>
2020-09-09 09:29:59 -0500

    :heavy_minus_sign: Removed Bootstrap
    No longer using bootstrap, wanted to give tailwindcss a try

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import { Container, Col, Row, Card } from 'react-bootstrap'
+
 import { getProjects } from '../lib/projects'
 
 export async function getStaticProps() {
@@ -20,60 +20,60 @@ export default function HomePage({ projectData }) {
         <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;1,400&display=swap" rel="stylesheet"></link>
       </Head>
       <section>
-        <Container>
+        <div>
           <h1 className="mt-4">Anthony Mercado</h1>
           <h2 className="mb-4">Software Engineer</h2>
-        </Container>
+        </div>
       </section>
       <section>
-        <Container>
+        <div>
           <hr/>
-          <Row>
-            <Col className="text-center">
+          <div>
+            <div className="text-center">
               <Link href="/blog">
               <a target="_blank">Blog</a>
               </Link>
-            </Col>
-            <Col className="text-center">
+            </div>
+            <div className="text-center">
               <a href="https://github.com/basedantoni" target="_blank">Github</a>        
-            </Col>
-            <Col className="text-center">
+            </div>
+            <div className="text-center">
               <a href="https://www.linkedin.com/in/anthony-mercado/" target="_blank">LinkedIn</a>
-            </Col>
-          </Row>
+            </div>
+          </div>
           <hr />
-        </Container>
+        </div>
       </section>
       <section>
-        <Container>
-          <Row>
-            <Col>
+        <div>
+          <div>
+            <div>
               <h3 className="mt-4" >Hello World,</h3>
               <p>I am an eager software engineer open to any opportunity presented to me. I have a passion for not only software development, but for design as well. I enjoy working with complex systems with many moving parts and piecing them together to work elegantly. My favorite technologies are React, NodeJS, and GraphQL</p>
-            </Col>
-            <Col className="my-auto text-center">
+            </div>
+            <div className="my-auto text-center">
               {/* <img 
                 className={`${utilStyles.headerImage} ${utilStyles.borderCircle}`} 
                 src="/images/profile.png"
                 alt="Anthony Mercado"
               /> */}
-            </Col>
-          </Row>
-          <Row>
-            <Col>
+            </div>
+          </div>
+          <div>
+            <div>
               <h3 className="mt-5">Get in Touch!</h3>
               <a className="px-3">Email</a>
               <a className="px-3" href="/files/resume.pdf" download="anthonymercado.pdf">Resume</a>
-            </Col>
-          </Row>
+            </div>
+          </div>
           <hr />
-        </Container>
+        </div>
       </section>
       <section>
-        <Container>
+        <div>
           <h3 className="text-center">Portfolio</h3>
-          <Row>
-            {projectData.map(({ id, title, shortDescription, fullDescription, image }) => (
+          <div>
+            {/* {projectData.map(({ id, title, shortDescription, fullDescription, image }) => (
               <Card key={id} style={{ width: '18rem' }} className="m-2">
                 <Card.Img variant="top" src={image.url} />
                 <Card.Body>
@@ -81,9 +81,9 @@ export default function HomePage({ projectData }) {
                   <Card.Text>{ shortDescription }</Card.Text>
                 </Card.Body>
               </Card>
-            ))}
-          </Row>
-        </Container>
+            ))} */}
+          </div>
+        </div>
       </section>
     </div>
   )
