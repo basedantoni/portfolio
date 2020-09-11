695d6c2f6a4b8cc2c8fd4f61dd1fdcc915852ac3
basedantoni <anthony.mercado300@gmail.com>
2020-09-11 13:33:17 -0500

    :lipstick: SVGs added to Navbar
    Navbar links are now represented by svg icons

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function HomePage({ projectData }) {
         <nav className="flex flex-row justify-center items-center">
           <div className="text-center px-4 py-2 m-3">
             <Link href="/blog">
-            <a className="text-purple-400 hover:text-purple-300" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-purple-400 hover:text-purple-300" viewBox="0 0 24 24"><path d="M1.438 16.872l-1.438 7.128 7.127-1.438 12.642-12.64-5.69-5.69-12.641 12.64zm2.271 2.253l-.85-.849 11.141-11.125.849.849-11.14 11.125zm20.291-13.436l-2.817 2.819-5.69-5.691 2.816-2.817 5.691 5.689z"/></svg></a>
+            <a target="_blank"><svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-purple-400 hover:text-purple-300" viewBox="0 0 24 24"><path d="M1.438 16.872l-1.438 7.128 7.127-1.438 12.642-12.64-5.69-5.69-12.641 12.64zm2.271 2.253l-.85-.849 11.141-11.125.849.849-11.14 11.125zm20.291-13.436l-2.817 2.819-5.69-5.691 2.816-2.817 5.691 5.689z"/></svg></a>
             </Link>
           </div>
           <div className="text-center px-4 py-2 m-3">
