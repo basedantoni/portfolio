88f1152f81bf246c7e5f22690abce03d214f871c
basedantoni <anthony.mercado300@gmail.com>
2020-09-11 13:31:09 -0500

    :lipstick: SVG
    SVGs added to navbar links

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,16 @@
 export default function Card({ project }) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg my-8 bg-gray-800">
+    <div className="max-w-sm rounded overflow-hidden shadow-lg m-5 bg-gray-800">
       <img className="w-full text-gray-300" src={project.image.url} alt={project.image.fileName}/>
-      <div className="my-3 p-6">
+      <div className="my-3 px-6">
         <h3 className="text-gray-300 text-2xl text-left pt-4">{project.title}</h3>
         <a className="text-purple-400 hover:text-purple-300" href={project.link} target="_blank">{project.link}</a>
         <p className="text-gray-300 py-4">{project.shortDescription}</p>
+        <div className=" pt-4 pb-2">
+          {project.technologies.map(tech => (
+            <span className="inline-block bg-gray-700 rounded-full px-2 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2">#{tech}</span>
+          ))}
+        </div>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -23,23 +23,23 @@ export default function HomePage({ projectData }) {
       </Head>
       <section className="mx-auto">
         <div className="container mx-auto m-8 px-2 py-1">
-          <h1 className="text-purple-500 text-4xl">Anthony Mercado</h1>
-          <h2 className="text-gray-300 text-3xl">Software Engineer</h2>
+          <h1 className="text-purple-500 text-3xl">Anthony Mercado</h1>
+          <h2 className="text-gray-300 text-2xl">Software Engineer</h2>
         </div>
       </section>
       <section>
         <hr />
-        <nav className="flex flex-row justify-center">
+        <nav className="flex flex-row justify-center items-center">
           <div className="text-center px-4 py-2 m-3">
             <Link href="/blog">
-            <a className="text-purple-400 hover:text-purple-300" target="_blank">Blog</a>
+            <a className="text-purple-400 hover:text-purple-300" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-purple-400 hover:text-purple-300" viewBox="0 0 24 24"><path d="M1.438 16.872l-1.438 7.128 7.127-1.438 12.642-12.64-5.69-5.69-12.641 12.64zm2.271 2.253l-.85-.849 11.141-11.125.849.849-11.14 11.125zm20.291-13.436l-2.817 2.819-5.69-5.691 2.816-2.817 5.691 5.689z"/></svg></a>
             </Link>
           </div>
           <div className="text-center px-4 py-2 m-3">
-            <a className="text-purple-400 hover:text-purple-300" href="https://github.com/basedantoni" target="_blank">Github</a>        
+            <a href="https://github.com/basedantoni" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-purple-400 hover:text-purple-300" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg></a>        
           </div>
           <div className="text-center px-4 py-2 m-3">
-            <a className="text-purple-400 hover:text-purple-300" href="https://www.linkedin.com/in/anthony-mercado/" target="_blank">LinkedIn</a>
+            <a href="https://www.linkedin.com/in/anthony-mercado/" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" className="h-8 w-8 fill-current text-purple-400 hover:text-purple-300" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg></a>
           </div>
         </nav>
       </section>
@@ -72,18 +72,11 @@ export default function HomePage({ projectData }) {
       <section>
         <div className="mt-4">
           <h3 className="text-gray-300 text-center text-4xl">Portfolio</h3>
-              {projectData.map(project => (
-                <Card project={project}></Card>
-              ))}
-            {/* {projectData.map(({ id, title, shortDescription, fullDescription, image }) => (
-              <Card key={id} style={{ width: '18rem' }} className="m-2">
-                <Card.Img variant="top" src={image.url} />
-                <Card.Body>
-                  <Card.Title>{ title }</Card.Title>
-                  <Card.Text>{ shortDescription }</Card.Text>
-                </Card.Body>
-              </Card>
-            ))} */}
+          <div className="flex flex-wrap">
+            {projectData.map(project => (
+                <Card key={project.id} project={project}></Card>
+            ))}
+          </div>
         </div>
       </section>
     </div>
